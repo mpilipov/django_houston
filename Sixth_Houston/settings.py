31dc25d9dbfcfb11a967fee7ee44c113bcefa8f4
mpilipov <misha7538@live.ru>
2022-01-28 18:03:39 +0300

    Added files and changes required for deployment to heroku

--- Sixth_Houston/settings.py
+++ Sixth_Houston/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-hb*d7w%+d3njzvoktgv9=6vag_^0$l4x!^g^tu7!9dvyybp2s5'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9taxa3cup@1$8obt2_+')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 # False, в том случае, если переменная окружения DJANGO_DEBUG будет проинициализирована пустой строкой, то есть,
